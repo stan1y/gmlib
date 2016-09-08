2343c1d27cccabec6555aee699422e6e9629de57
Stanislav Yudin <stan@endlessinsomnia.com>
2016-09-08 23:39:10 +1000

    overhaul resources framework

--- RESLib.cpp
+++ RESLib.cpp
@@ -1,80 +1,224 @@
 #include "RESLib.h"
+
+#include <boost/algorithm/string/predicate.hpp>
 #include <boost/filesystem.hpp>
 
-using namespace boost::filesystem;
+
+namespace resources
+{
+
+/* item in cache */
+struct resource_descriptor 
+{
+  iresource * resource;
+  std::string resource_name;
+  std::string resource_postfix;
+  boost::filesystem::path resource_path;
+
+  void init(const std::string & resource_id,
+            const std::string & p = std::string(),
+            iresource * r = nullptr)
+  {
+    resource = r;
+    resource_postfix = std::string("");
+    resource_name = std::string(resource_id);
+    resource_path = boost::filesystem::path(p);
+
+    if (resource_name.find(':') != std::string::npos) {
+      std::vector<std::string> parts;
+      split_string(resource_name, ':', parts);
+      resource_name = parts[0];
+      resource_postfix = parts[1];
+    }
+  }
+
+  resource_descriptor(const std::string & resource_id,
+                      const std::string & p = std::string(),
+                      iresource * r = nullptr)
+  {
+    init(resource_id, p, r);
+  }
+
+};
+
+typedef std::map<std::string, resource_descriptor> cache;
 
 /* Static pointer to assets folder */
-static path * _GM_AssetsRoot;
-typedef std::map<std::string, void*> cache;
-static cache* _GM_Cache = NULL;
+static boost::filesystem::path * g_assets_root;
+static cache* g_cache = nullptr;
 
-void RES_Init(const std::string& assets_root)
+void initialize(const std::string& assets_root)
 {
-  if ( !path(assets_root).is_absolute() ) {
-    path cwd(GM_GetCurrentPath());
-    cwd /= assets_root;
-    _GM_AssetsRoot = new path(cwd);
+  auto assets = boost::filesystem::path(assets_root);
+  if ( !assets.is_absolute() ) {
+    g_assets_root = new boost::filesystem::path(
+      boost::filesystem::current_path() / assets);
   }
   else {
-    _GM_AssetsRoot = new path(assets_root);
+    g_assets_root = new boost::filesystem::path(assets);
   }
-  _GM_Cache = new cache();
-  SDL_Log("RES_Init(): assets ready at [%s]", _GM_AssetsRoot->string().c_str());
+
+  if (!boost::filesystem::is_directory(*g_assets_root)) {
+    SDLEx_LogError("resources::initialize() - assets root is not a directory '%s'",
+      g_assets_root->string().c_str());
+    throw std::exception("Assets root is not a directory");
+  }
+
+  g_cache = new cache();
+  SDL_Log("resources::initialize() - assets ready at '%s'",
+    g_assets_root->string().c_str());
 }
 
-std::string RES_GetAssetsRoot()
+std::string root_path()
 {
-  return _GM_AssetsRoot->string();
+  if (g_assets_root != nullptr)
+    return g_assets_root->string();
+
+  SDLEx_LogError("resources::root_path() - no assets root initialized");
+  throw std::exception("no assets root");
 }
 
-void* RES_Get(const std::string& id)
+/* Get item from cache by path. Returns nullptr if not found */
+iresource * get(const std::string & resource)
 {
-    if (_GM_Cache == NULL) {
-        SDLEx_LogError("RES_Get() - not initialized");
-        throw std::exception("Resources are not initialized");
+  if (!g_cache || !g_assets_root) {
+        SDLEx_LogError("resources::get_texture() - no assets root initialized");
+        throw std::exception("no assets root");
     }
-    cache::iterator it = _GM_Cache->find(id);
-    if (it == _GM_Cache->end()) {
+    cache::iterator it = g_cache->find(resource);
+    if (it == g_cache->end()) {
         return nullptr;
     }
-    return it->second;
+    return it->second.resource;
 }
 
-void RES_Put(const std::string& id, void* res)
+/* Get texture from cache by path. Returns nullptr if not found */
+const texture & get_texture(const std::string& resource)
 {
-    if (res == NULL) {
-        SDLEx_LogError("RES_Put() - invalid item given");
-        throw std::exception("Invalid cache item given");
+  iresource * res = get(resource);
+  if (res == nullptr) {
+    std::string found_at = find(resource);
+    if (found_at.size()) {
+      res = new texture(found_at);
+      g_cache->insert(std::make_pair(resource, resource_descriptor(resource, found_at, res)));
     }
-    if (_GM_Cache == NULL) {
-        SDLEx_LogError("RES_Put() - not initialized");
-        throw std::exception("Resources are not initialized");
-    }
-    cache::iterator it = _GM_Cache->find(id);
-    if (it != _GM_Cache->end()) {
-        SDLEx_LogError("RES_Put() - item already in cache. id=%s, ptr=%p", 
-            it->first.c_str(), it->second);
-        throw std::exception("Item already in cache");
-    }
-    _GM_Cache->insert(std::make_pair(id, res));
+  }
+
+  if (res == nullptr) {
+    SDLEx_LogError("resources::get_texture - failed find texture '%s'", 
+      resource.c_str());
+    throw std::exception("Failed to find texture");
+  }
+
+  texture * tx = dynamic_cast<texture*> (res);
+  if (tx == nullptr) {
+    SDLEx_LogError("resources::get_texture - failed to cast iresource");
+    throw std::exception("Failed to cast iresource");
+  }
+  tx->incref();
+  return (*tx);
 }
 
-std::string RES_GetFullPath(const std::string& relPath)
+/* Get data from cache by path. Returns nullptr if not found */
+const data & get_data(const std::string& resource)
 {
-  path rel(relPath);
-  //check if path is abs
-  if (rel.is_absolute()) return relPath;
-  //combite relative path with assets root
-  path root(_GM_AssetsRoot ? _GM_AssetsRoot->string() : ".");
-  root /= rel;
-  return root.string();
+  iresource * res = get(resource);
+  if (res == nullptr) {
+    std::string found_at = find(resource);
+    if (found_at.size()) {
+      res = new data(found_at);
+      g_cache->insert(std::make_pair(resource, resource_descriptor(resource, found_at, res)));
+    }
+  }
+
+  if (res == nullptr) {
+    SDLEx_LogError("resources::get_texture - failed find texture '%s'", 
+      resource.c_str());
+    throw std::exception("Failed to find texture");
+  }
+
+  data * d = dynamic_cast<data*> (res);
+  if (d == nullptr) {
+    SDLEx_LogError("resources::get_data - failed to cast iresource");
+    throw std::exception("Failed to cast iresource");
+  }
+  d->incref();
+  return (*d);
 }
 
-bool RES_CheckExists(const std::string& relPath)
+const ttf_font & get_font(const std::string& resource)
 {
-  path itm(_GM_AssetsRoot ? _GM_AssetsRoot->string() : ".");
-  path rel(relPath);
-  if (rel.is_absolute()) return exists(rel);
-  itm /= rel;
-  return exists(itm);
+  iresource * res = get(resource);
+  if (res == nullptr) {
+    std::string found_at = find(resource);
+    if (found_at.size()) {
+      res = new texture(found_at);
+      g_cache->insert(std::make_pair(resource, resource_descriptor(resource, found_at, res)));
+    }
+  }
+
+  if (res == nullptr) {
+    SDLEx_LogError("resources::get_texture - failed find texture '%s'", 
+      resource.c_str());
+    throw std::exception("Failed to find texture");
+  }
+
+  ttf_font * f = dynamic_cast<ttf_font*> (res);
+  if (f == nullptr) {
+    SDLEx_LogError("resources::get_font - failed to cast iresource");
+    throw std::exception("Failed to cast iresource");
+  }
+  f->incref();
+  return (*f);
 }
+
+static bool find_file(const boost::filesystem::path & directory, 
+                      const boost::filesystem::path & file_name,
+                      boost::filesystem::path & output) 
+{
+  const boost::filesystem::recursive_directory_iterator end;
+  const auto it = std::find_if(boost::filesystem::recursive_directory_iterator(directory), end,
+                          [&file_name](const boost::filesystem::directory_entry& e) {
+                            return e.path().filename() == file_name;
+                          });
+  if (it == end) {
+    return false;
+  } else {
+    output = it->path();
+    return true;
+  }
+}
+
+std::string find(const std::string& resource)
+{
+  if (!g_cache || !g_assets_root) {
+      SDLEx_LogError("resources::find() - no assets root initialized");
+      throw std::exception("no assets root");
+  }
+
+  resource_descriptor descr(resource);
+  boost::filesystem::path resource_name(descr.resource_name);
+  boost::filesystem::path resource_filepath;
+  if (!find_file(*g_assets_root, resource_name, resource_filepath)) {
+    SDL_Log("resources::find - nothing found for resource name '%s'", resource.c_str());
+    return std::string();
+  }
+
+  std::string abs_path = boost::filesystem::absolute(resource_filepath, *g_assets_root).string();
+  SDL_Log("resources::find - found '%s' at '%s'", 
+    resource.c_str(),
+    abs_path.c_str());
+  
+  return abs_path;
+}
+
+bool exists(const std::string& resource)
+{
+  if (!g_cache || !g_assets_root) {
+      SDLEx_LogError("resources::exists() - no assets root initialized");
+      throw std::exception("no assets root");
+  }
+  return g_cache->find(resource) != g_cache->end() ? true : false;
+}
+
+}; //namespace resources
--- RESLib.h
+++ RESLib.h
@@ -2,21 +2,51 @@
 #define _RES_LIB_H
 
 #include "GMLib.h"
+#include "GMTexture.h"
+#include "GMData.h"
+
+namespace resources
+{
+
+/*
+ * Resource ID Format
+ *
+ * A Resource ID is a string with format of [resource_name:resource_postfix].
+ * A resource_name segment is a unique filename of the resource in the assets storage.
+ * A resource_postfix is a an optional string witch might be used by certain resource
+ * loaders, such as fonts.
+ * Example:
+ * resources::get_texture("cat.png");
+ * resources::get_data("hero.json");
+ * resources::get_font("comicsans.ttf:22");
+ * resources::find("comicsans.ttf:22") 
+ *  -> "/path/to/assets/ui/fonts/comicsans.ttf"
+ */
 
 /* Get item from cache by path. Returns nullptr if not found */
-void* RES_Get(const std::string& id);
+iresource * get(const std::string & resource);
 
-/* Append item to cache. Raises exception if item already there */
-void RES_Put(const std::string& id, void* res);
+/* Get texture from cache by path */
+const texture & get_texture(const std::string& resource);
+
+/* Get texture from cache by path */
+const data & get_data(const std::string& resource);
+
+/* Get texture from cache by path */
+const ttf_font & get_font(const std::string& resource);
 
 /* Initialize resources root */
-void RES_Init(const std::string& assets_root);
-std::string RES_GetAssetsRoot();
+void initialize(const std::string& assets_root);
 
-/* Return full path to a resource file */
-std::string RES_GetFullPath(const std::string& relPath);
+/* Get path to the assets root */
+std::string root_path();
+
+/* Recursively lookup assets to match file names with give resource id */
+std::string find(const std::string& resource);
 
 /* Test if resource file present */
-bool RES_CheckExists(const std::string& relPath);
+bool exists(const std::string& resource);
+
+}
 
 #endif //_RES_LIB_H
