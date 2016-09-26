a497a87f02b5b6aa1196f5bf394af31c710c54a8
Stanislav Yudin <stan@endlessinsomnia.com>
2016-09-26 14:36:16 +1000

    implement all parts of push btn

--- GMUIPushButton.cpp
+++ GMUIPushButton.cpp
@@ -1,65 +1,146 @@
 #include "GMUIPushButton.h"
+#include "RESLib.h"
 
 namespace ui {
 
-push_button::push_button(rect pos,
+push_btn::push_btn(std::string idle_icon_file_path,
+                         std::string disabled_icon_file_path,
+                         std::string pressed_icon_file_path):
+control("push_btn", rect()),
+  _is_pressed(false),
+  _sticky(false)
+{
+  // load primary idle icon
+  if (idle_icon_file_path.length() <= 0) {
+    SDLEx_LogError("push_btn::push_btn - no idle icon specified for push button");
+    throw std::exception("No idle icon specified for push button");
+  }
+  set_idle_icon(idle_icon_file_path);
+
+  // update own size to reflect idle frame sprite
+  const texture & body = get_frame()->idle;
+  set_pos(rect(pos().x, pos().y, body.width(), body.height()));
+
+  // try to load optional icons
+  if (disabled_icon_file_path.length() > 0)
+    set_disabled_icon(disabled_icon_file_path);
+  if (pressed_icon_file_path.length() > 0)
+    set_pressed_icon(pressed_icon_file_path);
+
+  mouse_up += boost::bind( &push_btn::on_mouse_up, this, _1 );
+  mouse_down += boost::bind( &push_btn::on_mouse_down, this, _1 );
+}
+
+push_btn::push_btn(rect pos,
                          std::string idle_file_path,
-                         std::string hovered_file_path,
+                         std::string disabled_file_path,
                          std::string pressed_file_path):
-  control("pushbtn", pos),
-  _pressed(false),
+  control("push_btn", pos),
+  _is_pressed(false),
   _sticky(false)
 {
   // load primary idle icon
   if (idle_file_path.length() <= 0) {
-    SDLEx_LogError("push_button::push_button - no idle icon specified for push button");
+    SDLEx_LogError("push_btn::push_btn - no idle icon specified for push button");
     throw std::exception("No idle icon specified for push button");
   }
   set_idle_icon(idle_file_path);
   // try to load optional icons
-  if (hovered_file_path.length() > 0)
-    set_hoverd_icon(hovered_file_path);
+  if (disabled_file_path.length() > 0)
+    set_disabled_icon(disabled_file_path);
   if (pressed_file_path.length() > 0)
     set_pressed_icon(pressed_file_path);
+
+  mouse_up += boost::bind( &push_btn::on_mouse_up, this, _1 );
+  mouse_down += boost::bind( &push_btn::on_mouse_down, this, _1 );
 }
 
-push_button::~push_button()
+push_btn::~push_btn()
 {
 }
 
-void push_button::set_idle_icon(const std::string & file_path)
+void push_btn::on_mouse_down(control * target)
 {
-  _idle.load(file_path);
+  _is_pressed = true;
 }
 
-void push_button::set_idle_icon(SDL_Texture* tex)
+void push_btn::on_mouse_up(control * target)
 {
-  _idle.set_texture(tex);
+  _is_pressed = false;
 }
 
-void push_button::set_hoverd_icon(const std::string & file_path)
+void push_btn::set_idle_icon(const std::string & file_path)
 {
-  _hovered.load(file_path);
+  _idle_icon.load(file_path);
 }
 
-void push_button::set_hovered_icon(SDL_Texture* tex)
+void push_btn::set_idle_icon(SDL_Texture* tex)
 {
-  _hovered.set_texture(tex);
+  _idle_icon.set_texture(tex);
 }
 
-void push_button::set_pressed_icon(const std::string & file_path)
+void push_btn::set_disabled_icon(const std::string & file_path)
 {
-  _pressed.load(file_path);
+  _disabled_icon.load(file_path);
 }
 
-void push_button::set_pressed_icon(SDL_Texture* tex)
+void push_btn::set_disabled_icon(SDL_Texture* tex)
 {
-  _pressed.set_texture(tex);
+  _disabled_icon.set_texture(tex);
 }
 
-void push_button::load(const data & d)
+void push_btn::set_pressed_icon(const std::string & file_path)
 {
-  
+  _pressed_icon.load(file_path);
+}
+
+void push_btn::set_pressed_icon(SDL_Texture* tex)
+{
+  _pressed_icon.set_texture(tex);
+}
+
+void push_btn::load(const data & d)
+{
+  if (!d.has_key("icon_idle")) {
+    SDLEx_LogError("push_btn::load - no idle icon specified");
+    throw std::exception("No idle icon specified for push_btn");
+  }
+  set_idle_icon(resources::find(d["icon_idle"].as<std::string>()));
+
+  if (d.has_key("icon_disabled"))
+    set_disabled_icon(resources::find(d["icon_disabled"].as<std::string>()));
+
+  if (d.has_key("icon_pressed"))
+    set_pressed_icon(resources::find(d["icon_pressed"].as<std::string>()));
+
+  if (d.has_key("sticky") && d["sticky"].as<bool>()) {
+    set_sticky(true);
+  }
+}
+
+void push_btn::render(SDL_Renderer* r, const rect & dst)
+{
+  if (disabled()) {
+    if(get_frame()->disabled.is_valid()) 
+      get_frame()->disabled.render(r, dst);
+    if(_disabled_icon.is_valid())
+      _disabled_icon.render(r, dst.center(_disabled_icon.width(), _disabled_icon.height()));
+    else
+      _idle_icon.render(r, dst.center(_idle_icon.width(), _idle_icon.height()));
+  }
+  else {
+    if (_is_pressed) { 
+      if(get_frame()->pressed.is_valid()) 
+        get_frame()->pressed.render(r, dst);
+      if(_pressed_icon.is_valid())
+        _pressed_icon.render(r, dst.center(_pressed_icon.width(), _pressed_icon.height()));
+      else
+        _idle_icon.render(r, dst.center(_idle_icon.width(), _idle_icon.height()));
+    } else {
+      get_frame()->idle.render(r, dst);
+      _idle_icon.render(r, dst.center(_idle_icon.width(), _idle_icon.height()));
+    }
+  }
 }
 
 }; //namespace ui
--- GMUIPushButton.h
+++ GMUIPushButton.h
@@ -5,14 +5,22 @@
 
 namespace ui {
 
-class push_button : public control {
+class push_btn : public control {
 public:
 
-  push_button(rect pos, 
+  // create push_btn with specific rect
+  push_btn(rect pos, 
               std::string idle_file_path,
-              std::string hovered_file_path = "",
+              std::string disabled_file_path = "",
               std::string pressed_file_path = "");
-  virtual ~push_button();
+
+  // create push_btn with size of the idle icon
+  push_btn(std::string idle_file_path,
+              std::string disabled_file_path = "",
+              std::string pressed_file_path = "");
+
+
+  virtual ~push_btn();
 
   bool is_sticky() { return _sticky; }
   void set_sticky(bool s) { _sticky = s; }
@@ -20,29 +28,33 @@ public:
   void set_idle_icon(const std::string & file_path);
   void set_idle_icon(SDL_Texture* tex);
 
-  void set_hoverd_icon(const std::string & file_path);
-  void set_hovered_icon(SDL_Texture* tex);
+  void set_disabled_icon(const std::string & file_path);
+  void set_disabled_icon(SDL_Texture* tex);
 
   void set_pressed_icon(const std::string & file_path);
   void set_pressed_icon(SDL_Texture* tex);
 
-  bool is_pressed() { return _press; }
-
   virtual void load(const data &);
+  virtual void render(SDL_Renderer* r, const rect & dst);
 
 private:
 
-  void on_hover(control * target);
+  const theme::push_button_frame * get_frame() const
+  {
+    return dynamic_cast<const theme::push_button_frame*> (current_theme().get_frame("pushbtn"));
+  }
+
+  void on_disabled(control * target);
   void on_hover_lost(control * target);
   void on_mouse_down(control * target);
   void on_mouse_up(control * target);
 
   bool _sticky;
-  bool _press;
+  bool _is_pressed;
 
-  texture _idle;
-  texture _hovered;
-  texture _pressed;
+  texture _idle_icon;
+  texture _disabled_icon;
+  texture _pressed_icon;
 };
 
 }; //namespace ui
